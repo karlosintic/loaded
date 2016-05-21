67c5935b1ab66c3ac62df0c6f1ef445597f71ba2
Karlo Sintic <karlo.sintic@gmail.com>
2016-05-21 21:51:31 +0200

    update

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function update() {
 		mil = new Date().getTime() - then.getTime(),
 		sectoday = Math.round(mil / 1000),
 		sec = (day * 86400) + sectoday,
-		year = now.getFullYear(),
+		year = now.getFullYear()+1,
 		secsum = year % 4 === 0 && year % 100 !== 0 || year % 400 === 0 ? 31622400 : 31536000;
 
 	per = Math.floor((sec / secsum) * 100);
